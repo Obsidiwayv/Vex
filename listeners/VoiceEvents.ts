3fb7d0938ebad30a979a08d4d6a3a3170dbaa764
Obsidiwayv <pizza5803@gmail.com>
2024-10-08 18:09:36 -0400

    change pings to a function

--- listeners/VoiceEvents.ts
+++ listeners/VoiceEvents.ts
@@ -10,15 +10,38 @@ export async function voiceJoin(
 ) {
   const pings = 3;
   const role = readKey("ROLE");
-  const ping_role = readKey("PING");
-  const channel_id = readKey("CHNL");
-  const msg = `<@&${ping_role.str()}>`;
+  const ping_role_reg = readKey("PING");
+  const channel_id_reg = readKey("CHNL");
+  const ping_role_ad = readKey("AD_PING_ROLE");
+  const channel_id_ad = readKey("AD_CHNL");
 
-  if (member.roles.includes(role.str()) && channel.voiceMembers.size === 1) {
-    for (let i = 0; i < pings; i++) {
+  const is_admin = channel.id === channel_id_ad.str();
+
+  pingRole(
+    role.str(),
+    is_admin ? channel_id_ad.str() : channel_id_reg.str(),
+    is_admin ? ping_role_ad.str() : ping_role_reg.str(),
+    { channel, client, member },
+  );
+}
+
+interface pingOptions {
+  member: Eris.Member;
+  channel: Eris.AnyVoiceChannel;
+  client: Eris.Client;
+}
+
+async function pingRole(
+  role: string,
+  channel_id: string,
+  ping_role: string,
+  { channel, client, member }: pingOptions,
+) {
+  if (member.roles.includes(role) && channel.voiceMembers.size === 1) {
+    for (let i = 0; i < 3; i++) {
       client.createMessage(
-        channel_id.str(),
-        `${msg}, \`${member.username}\` is in <#${channel.id}>`,
+        channel_id,
+        `<@&${ping_role}>, \`${member.username}\` is in <#${channel.id}>`,
       );
       await sleep(5000);
     }
