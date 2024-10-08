dd4f4f0e318ce2ec109ac2d3516d3ab4f83a43fc
Obsidiwayv <pizza5803@gmail.com>
2024-10-08 19:27:22 -0400

    Make sure bots dont get member role

--- listeners/MemberJoin.ts
+++ listeners/MemberJoin.ts
@@ -5,7 +5,9 @@ import { readKey } from "../config/config.reader";
 export default function (guild: Eris.Guild, member: Eris.Member) {
   const role_id = readKey("ROLE_JOIN");
   try {
-    member.addRole(role_id.str());
+    if (!member.bot) {
+      member.addRole(role_id.str());
+    }
   } catch {
     debug("Unable to add join role to member");
   }
