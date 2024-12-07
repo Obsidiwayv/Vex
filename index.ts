6001a4370d0ba8ed3d8f3b8efae7d0525b2af140
Obsidiwayv <pizza5803@gmail.com>
2024-12-07 13:35:22 -0500

    change server.ts again

--- index.ts
+++ index.ts
@@ -2,11 +2,9 @@ import Eris, { Client } from "eris";
 import { debugMode, readKey } from "./config/config.reader";
 import { crashReport, debug, log } from "./logger";
 import { voiceJoin } from "./listeners/VoiceEvents";
-import { runServer } from "./server";
 import { isEnabled } from "./check";
-
 if (isEnabled(readKey("ENABLE_SERVER").str())) {
-  runServer();
+  require("./server");
 }
 import MessageCreateListener from "./listeners/MessageCreateListener";
 import { checkAliases, RegisterCommand } from "./CommandRegistry";
--- server.ts
+++ server.ts
@@ -2,19 +2,18 @@ import Fastify from "fastify";
 import { readKey } from "./config/config.reader";
 import GithubListener from "./listeners/GithubListener";
 
-export function runServer() {
-  const app = Fastify({ logger: true });
-  const port = readKey("PORT");
-  const host = readKey("HOST");
 
-  app.post("/webhooks/post", (req, res) => {
-    if (!GithubListener.validate(req)) {
-      return res.status(400).send("Invalid request");
-    }
-    GithubListener.handle(req, res);
-    // Make sure github gets the code
-    res.status(202).send("Accepted");
-  });
+const app = Fastify({ logger: true });
+const port = readKey("PORT");
+const host = readKey("HOST");
 
-  app.listen({ port: port.int(), host: host.str() });
-};
+app.post("/webhooks/post", (req, res) => {
+  if (!GithubListener.validate(req)) {
+    return res.status(400).send("Invalid request");
+  }
+  GithubListener.handle(req, res);
+  // Make sure github gets the code
+  res.status(202).send("Accepted");
+});
+
+app.listen({ port: port.int(), host: host.str() });
