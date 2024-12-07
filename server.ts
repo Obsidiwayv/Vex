b191d7137df595a7a3f31706cb85cefdd78cd3dc
Obsidiwayv <pizza5803@gmail.com>
2024-12-07 13:31:14 -0500

    format server.ts

--- server.ts
+++ server.ts
@@ -2,19 +2,19 @@ import Fastify from "fastify";
 import { readKey } from "./config/config.reader";
 import GithubListener from "./listeners/GithubListener";
 
-const app = Fastify({ logger: true });
-const port = readKey("PORT");
-const host = readKey("HOST");
-
-app.post("/webhooks/post", (req, res) => {
-  if (!GithubListener.validate(req)) {
-    return res.status(400).send("Invalid request");
-  }
-  GithubListener.handle(req, res);
-  // Make sure github gets the code
-  res.status(202).send("Accepted");
-});
-
 export function runServer() {
+  const app = Fastify({ logger: true });
+  const port = readKey("PORT");
+  const host = readKey("HOST");
+
+  app.post("/webhooks/post", (req, res) => {
+    if (!GithubListener.validate(req)) {
+      return res.status(400).send("Invalid request");
+    }
+    GithubListener.handle(req, res);
+    // Make sure github gets the code
+    res.status(202).send("Accepted");
+  });
+
   app.listen({ port: port.int(), host: host.str() });
 };
