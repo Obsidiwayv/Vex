d33ee4838678bdf0eef8cf53e0bf0a741c377979
Obsidiwayv <pizza5803@gmail.com>
2025-03-26 21:14:09 -0400

    update Gemini.ts

--- commands/Gemini.ts
+++ commands/Gemini.ts
@@ -1,10 +1,12 @@
 import type { Message } from "eris";
 import { BaseCommand, type CTX } from "./Base.ts";
 import { GeminiExtension } from "../Extensions.ts";
+import {GetEmoji} from "../config/Emoji.ts";
 
 export class GeminiCommand extends BaseCommand {
     public override async execute(message: Message, ctx: CTX) {
-        const bMsg = await message.channel.createMessage("Gemini is thinking...");
+        const bMsg =
+            await message.channel.createMessage(`Gemini is thinking... ${GetEmoji("gemini").Str()}`);
         const gemini = new GeminiExtension();
         const genAI = await gemini.createResponse(ctx.args.join(" "));
         await bMsg.edit(genAI.response.text());
