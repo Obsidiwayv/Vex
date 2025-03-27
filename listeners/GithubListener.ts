8da1a21bd2f1a06617eb7c43a2e842bd6b2cfe14
Obsidiwayv <pizza5803@gmail.com>
2025-03-26 20:51:50 -0400

    Remove log here

--- listeners/GithubListener.ts
+++ listeners/GithubListener.ts
@@ -14,7 +14,6 @@ async function runGitCommand() {
 export default class {
   static async handle(req: Request) {
     const response: any = await req.json();
-    console.log(response.ref);
     if (response.ref === "refs/heads/main") {
       this.createMessage(
         `Pulling \`${response.before.substring(0, 7)}\` -> \`${response.after.substring(0, 7)}\``,
